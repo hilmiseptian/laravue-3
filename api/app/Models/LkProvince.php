6b522fe25de9a6b4a743b466404b1748f15f4239
hilmi saftian <hilmi@esb.co.id>
2023-07-27 00:24:04 +0700

    [Migration] Add-province-and-city-table

---
+++ api/app/Models/LkProvince.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LkProvince extends Model
+{   
+    protected $table = 'lk_province';
+
+    use HasFactory;
+}
